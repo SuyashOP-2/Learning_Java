39cfc32e7381c360205516768a0f7e761efc9e61
SuyashOP-2 <suyashsshirtar2@gmail.com>
2023-06-04 17:09:16 +0530

    first commit

---
+++ 1.java
@@ -0,0 +1,39 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.print("Hello, World!");
+        // Variables
+        // String name = "aman";
+        // String neighbour = "akku";
+        // int age = 30;
+        
+        // String friend = neighbour;
+        
+        // System.out.print(friend);
+        
+        byte age = 30;
+        // int phone = 123334874000;
+        
+        // long phone1 = 123334874000L;
+        
+        //  System.out.print(phone1);
+         
+         float pi = 3.14F;
+         
+         System.out.print(pi);
+         char letter = '@';
+         
+         boolean isAdult = true;
+        //  boolean isAdult = true;
+        
+         String neighbour = "akku";
+          
+         System.out.println(neighbour.length());
+        
+        
+        
+
+    }
+}
